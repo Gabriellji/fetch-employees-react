3074f3a49598702a99e7d26248ac652f1020a351
Gabriellji <gabriellji@yahoo.com>
2020-11-15 01:13:56 +0300

    feat: add axios and get data method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import DisplayEmployee from './components/DisplayEmployee';
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      employee: sampleEmployee
+    }
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+  getEmployee() {
+    axios.get('https://randomuser.me/api?nat=en')
+    .then(response => response.data)
+    .then(data => {
+      this.setState({
+        employee: data.results[0]
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get employee</button>
+      </div>
+    );
+  };
 }
 
 export default App;
